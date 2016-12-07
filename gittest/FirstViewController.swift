6a4c63fd3fe92f26fd1eefbe46951cfc5dbab140
yui <yuiakao@YuisMac.local>
2016-12-07 17:20:19 +0900

    add:コメント

--- gittest/FirstViewController.swift
+++ gittest/FirstViewController.swift
@@ -14,6 +14,8 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //GITHUB!!!!!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
